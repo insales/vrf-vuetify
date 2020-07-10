f28146a4b86538c0f6d5125a39b8ab03bc6a0ebb
Dmitry Sadovsky <dimailn9@gmail.com>
2020-07-10 17:05:15 +0300

    remove flex

--- src/components/autocomplete.vue
+++ src/components/autocomplete.vue
@@ -1,48 +1,46 @@
 <template>
 
 
-<v-layout align-center>
-  <v-flex>
-    <v-menu
-      v-model="menu"
-      :open-on-click="false"
-      offset-y
-      offset-overflow
-      style="width: 100%"
-      content-class="v-autocomplete__content"
-      bottom
-      :max-height="300"
-    >
-      <template v-slot:activator="{ on }">
-        <v-text-field
-          v-model="query"
-          v-on="on"
-          @input="onInput"
-          :loading="loading"
-          :label="humanName"
-          :disabled="$disabled"
-          :clearable="clearable"
-          :error-messages="errors[name] && errors[name][0]"
-          :placeholder="placeholder"
-          @keydown="onKeydown"
-          @click="onClick"
-          @click:clear="onClear"
-          :autofocus="autofocus"
-          autocomplete="off"
-          ref="autocomplete"
-          :name="name"
-        />
-      </template>
-      <component :is="itemsComponent" :items="items" @select="onSelect" />
-      <!-- <v-list> -->
+<div class="d-flex">
+  <v-menu
+    v-model="menu"
+    :open-on-click="false"
+    offset-y
+    offset-overflow
+    style="width: 100%"
+    content-class="v-autocomplete__content"
+    bottom
+    :max-height="300"
+  >
+    <template v-slot:activator="{ on }">
+      <v-text-field
+        v-model="query"
+        v-on="on"
+        @input="onInput"
+        :loading="loading"
+        :label="humanName"
+        :disabled="$disabled"
+        :clearable="clearable"
+        :error-messages="errors[name] && errors[name][0]"
+        :placeholder="placeholder"
+        @keydown="onKeydown"
+        @click="onClick"
+        @click:clear="onClear"
+        :autofocus="autofocus"
+        autocomplete="off"
+        ref="autocomplete"
+        :name="name"
+      />
+    </template>
+    <component :is="itemsComponent" :items="items" @select="onSelect" />
+    <!-- <v-list> -->
 <!--           <v-list-item v-for="item, index in items" :key="index" @click="onSelect(item)">
-          <component :is="itemComponent"  :item="item" />
-        </v-list-item> -->
-      <!-- </v-list> -->
-    </v-menu>
-  </v-flex>
+        <component :is="itemComponent"  :item="item" />
+      </v-list-item> -->
+    <!-- </v-list> -->
+  </v-menu>
   <slot name="append-icon" />
-</v-layout>
+</div>
 
 
 </template>
