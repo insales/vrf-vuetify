81cb1ea9420ba1519f249dd75df74266e2f5b1c7
Dmitry Sadovsky <dimailn9@gmail.com>
2020-07-10 16:33:56 +0300

    autocomplete fix

--- src/components/autocomplete.vue
+++ src/components/autocomplete.vue
@@ -1,49 +1,50 @@
 <template>
 
-<v-flex :class="flexClasses">
-  <v-layout align-center>
-    <v-flex>
-      <v-menu
-        v-model="menu"
-        :open-on-click="false"
-        offset-y
-        offset-overflow
-        style="width: 100%"
-        content-class="v-autocomplete__content"
-        bottom
-        :max-height="300"
-      >
-        <template v-slot:activator="{ on }">
-          <v-text-field
-            v-model="query"
-            v-on="on"
-            @input="onInput"
-            :loading="loading"
-            :label="humanName"
-            :disabled="$disabled"
-            :clearable="clearable"
-            :error-messages="errors[name] && errors[name][0]"
-            :placeholder="placeholder"
-            @keydown="onKeydown"
-            @click="onClick"
-            @click:clear="onClear"
-            :autofocus="autofocus"
-            autocomplete="off"
-            ref="autocomplete"
-            :name="name"
-          />
-        </template>
-        <component :is="itemsComponent" :items="items" @select="onSelect" />
-        <!-- <v-list> -->
+
+<v-layout align-center>
+  <v-flex>
+    <v-menu
+      v-model="menu"
+      :open-on-click="false"
+      offset-y
+      offset-overflow
+      style="width: 100%"
+      content-class="v-autocomplete__content"
+      bottom
+      :max-height="300"
+    >
+      <template v-slot:activator="{ on }">
+        <v-text-field
+          v-model="query"
+          v-on="on"
+          @input="onInput"
+          :loading="loading"
+          :label="humanName"
+          :disabled="$disabled"
+          :clearable="clearable"
+          :error-messages="errors[name] && errors[name][0]"
+          :placeholder="placeholder"
+          @keydown="onKeydown"
+          @click="onClick"
+          @click:clear="onClear"
+          :autofocus="autofocus"
+          autocomplete="off"
+          ref="autocomplete"
+          :name="name"
+        />
+        <v-text-field />
+      </template>
+      <component :is="itemsComponent" :items="items" @select="onSelect" />
+      <!-- <v-list> -->
 <!--           <v-list-item v-for="item, index in items" :key="index" @click="onSelect(item)">
-            <component :is="itemComponent"  :item="item" />
-          </v-list-item> -->
-        <!-- </v-list> -->
-      </v-menu>
-    </v-flex>
-    <slot name="append-icon" />
-  </v-layout>
-</v-flex>
+          <component :is="itemComponent"  :item="item" />
+        </v-list-item> -->
+      <!-- </v-list> -->
+    </v-menu>
+  </v-flex>
+  <slot name="append-icon" />
+</v-layout>
+
 
 </template>
 
