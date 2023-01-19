3f22e08b93bdb44474e3c07d900279738ae42aa7
Dmitry Sadovsky <dimailn9@gmail.com>
2023-01-19 15:37:27 +0300

    new autocomplete support

--- src/components/autocomplete.vue
+++ src/components/autocomplete.vue
@@ -20,7 +20,6 @@
         :loading="loading"
         :label="humanName"
         :disabled="$disabled"
-        :clearable="clearable"
         :error-messages="$firstError"
         :placeholder="placeholder"
         @keydown="onKeydown"
@@ -33,12 +32,42 @@
         v-bind="$attrs"
       />
     </template>
-    <component :is="itemsComponent" :items="items" @select="onSelect" />
-    <!-- <v-list> -->
-<!--           <v-list-item v-for="item, index in items" :key="index" @click="onSelect(item)">
-        <component :is="itemComponent"  :item="item" />
-      </v-list-item> -->
-    <!-- </v-list> -->
+
+    <slot name="items" v-if="hasItemsSlot" :items="items" :$on="on()" />
+    <v-list v-else-if="hasItemSlot">
+      <slot
+        name="item"
+        v-for="item in items"
+        :item="item"
+        :$on="onFor(item)"
+        :text="presentItem(item)"
+      />
+    </v-list>
+
+    <component :is="itemsComponent" v-else-if="itemsComponent" />
+    <v-list v-else-if="itemComponent">
+      <component
+        :is="itemComponent"
+        v-for="item, i in items"
+        :item="item"
+        :$on="onFor(item)"
+        :text="presentItem(item)"
+        :key="i"
+      />
+    </v-list>
+
+    <v-list
+        v-for="item, i in items"
+        :key="i"
+        v-else
+      >
+      <v-list-item v-for="item, index in items" :key="index" @click="onSelect(item)">
+        <v-list-item-content>
+          <v-list-item-title v-text="presentItem(item)"></v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+
   </v-menu>
   <slot name="append-icon" />
 </div>
@@ -49,7 +78,7 @@
 
 <script>
 import {descriptors} from 'vrf'
-import {VTextField, VMenu} from 'vuetify/lib'
+import {VTextField, VMenu, VList, VListItem} from 'vuetify/lib'
 
 
 export default {
@@ -57,15 +86,13 @@ export default {
   inheritAttrs: false,
   components: {
     VTextField,
-    VMenu
-  },
-  props: {
-    messages: Array,
-    clearable: Boolean
+    VMenu,
+    VList,
+    VListItem
   },
 
   methods: {
-    onKeydown: (e) => {
+    onKeydown(e) {
       this.$emit('keydown', e)
     }
   }
